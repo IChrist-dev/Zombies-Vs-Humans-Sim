50a42f6fb431a262ec83932b2103b495ad592c89
Ian Christian <w0480449@nscc.ca>
2023-11-26 16:15:08 -0400

    Output slightly more consistent looking

--- City.cpp
+++ City.cpp
@@ -66,10 +66,6 @@ void City::move() {
 ostream &operator<<(ostream &output, City &city) {
     for (int i=0; i < GRIDSIZE; i++) {
         for (int j=0; j < GRIDSIZE; j++) {
-            if (j == GRIDSIZE - 1) {
-                output << "\n";
-            }
-
             if (city.grid[i][j] != nullptr) {
                 char orgChar = city.getOrganism(i, j)->getSpecies();
                 cout << orgChar;
@@ -79,6 +75,7 @@ ostream &operator<<(ostream &output, City &city) {
                 output << '_';
             }
         }
+        output << "\n";
     }
 
     return output;
