1f3552897a5c4b86140c5c506aa4a7d2f6137c95
Ian Christian <w0480449@nscc.ca>
2023-12-06 02:55:30 -0400

    Cleaned up main

--- main.cpp
+++ main.cpp
@@ -26,24 +26,13 @@ int main() {
 
     while (City::hasDiversity(*city) && city->generation < ITERATIONS) { //while both humans and zombies exist
         this_thread::sleep_for(interval);
+
         ClearScreen();
-
-
-// Alternate multi-pass version with each activity happening in its own
-// pass through the array. Lends itself to prototyping and testing:
-        //   city->humansMove();
-        //   city->zombiesMoveEat();
-        //   city->humansRecruit();
-        //   city->zombiesRecruit();
-        //   city->zombiesStarve();
-        //   city->countOrganisms(Z or H goes here);
-
-//        city->reset(); //resets moved flags
-//        city->countOrganisms(Z or H goes here);// run once for each type
         city->step(*city);
         cout << "GENERATION " << city->getGeneration() << endl;
         cout << "HUMANS: " << city->countType(*city, HUMAN_CH) << endl;
         cout << "ZOMBIES: " << city->countType(*city, ZOMBIE_CH) << endl;
+
     } // End while
 
     if (!city->hasDiversity(*city)) {
