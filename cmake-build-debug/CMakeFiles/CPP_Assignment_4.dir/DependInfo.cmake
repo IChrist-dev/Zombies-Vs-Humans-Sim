a31b41aa7437fa4c94ccf37264bc0273957bf5c9
Ian Christian <w0480449@nscc.ca>
2024-03-02 16:37:58 -0400

    Added static compile option

---
+++ cmake-build-debug/CMakeFiles/CPP_Assignment_4.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Ian/CLionProjects/CPP-Assignment_4/City.cpp" "CMakeFiles/CPP_Assignment_4.dir/City.cpp.obj" "gcc" "CMakeFiles/CPP_Assignment_4.dir/City.cpp.obj.d"
+  "C:/Users/Ian/CLionProjects/CPP-Assignment_4/Human.cpp" "CMakeFiles/CPP_Assignment_4.dir/Human.cpp.obj" "gcc" "CMakeFiles/CPP_Assignment_4.dir/Human.cpp.obj.d"
+  "C:/Users/Ian/CLionProjects/CPP-Assignment_4/Organism.cpp" "CMakeFiles/CPP_Assignment_4.dir/Organism.cpp.obj" "gcc" "CMakeFiles/CPP_Assignment_4.dir/Organism.cpp.obj.d"
+  "C:/Users/Ian/CLionProjects/CPP-Assignment_4/Zombie.cpp" "CMakeFiles/CPP_Assignment_4.dir/Zombie.cpp.obj" "gcc" "CMakeFiles/CPP_Assignment_4.dir/Zombie.cpp.obj.d"
+  "C:/Users/Ian/CLionProjects/CPP-Assignment_4/main.cpp" "CMakeFiles/CPP_Assignment_4.dir/main.cpp.obj" "gcc" "CMakeFiles/CPP_Assignment_4.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CPP_Assignment_4.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Assignment_4.dir/City.cpp.obj"
+  "CMakeFiles/CPP_Assignment_4.dir/City.cpp.obj.d"
+  "CMakeFiles/CPP_Assignment_4.dir/Human.cpp.obj"
+  "CMakeFiles/CPP_Assignment_4.dir/Human.cpp.obj.d"
+  "CMakeFiles/CPP_Assignment_4.dir/Organism.cpp.obj"
+  "CMakeFiles/CPP_Assignment_4.dir/Organism.cpp.obj.d"
+  "CMakeFiles/CPP_Assignment_4.dir/Zombie.cpp.obj"
+  "CMakeFiles/CPP_Assignment_4.dir/Zombie.cpp.obj.d"
+  "CMakeFiles/CPP_Assignment_4.dir/main.cpp.obj"
+  "CMakeFiles/CPP_Assignment_4.dir/main.cpp.obj.d"
+  "CPP_Assignment_4.exe"
+  "CPP_Assignment_4.exe.manifest"
+  "CPP_Assignment_4.pdb"
+  "libCPP_Assignment_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Assignment_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
